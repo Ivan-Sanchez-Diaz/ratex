06001ea2fd446c2288c0a7e34038f829307a9b73
Ivan-Sanchez-Diaz <ivansanchezdiaz2001@gmail.com>
2023-09-18 01:33:39 -0500

    feat: hex to octal

--- src/main.rs
+++ src/main.rs
@@ -1,143 +1,140 @@
+use std::slice::Iter;
+
 fn main() {
-    hex_to_decimal("FF45A8");
-    hex_to_decimal("FDA897");
-    hex_to_decimal("ABC123");
-    hex_to_decimal("FFFFF");
-    hex_to_decimal("3B5D8F");
-    hex_to_decimal("789DEF");
-    hex_to_decimal("2015FFAAB");
+    hex_to_octal("65FE8");
 }
 
 pub fn hex_to_decimal(hex: &str) {
     let mut raw = String::new();
 
-    raw.push_str(r"\begin{center}");
-    raw.push_str("\n");
-    raw.push_str(&format!("hexadecimal={}", hex));
-    raw.push_str(r" \\");
-    raw.push_str("\n");
-    raw.push_str(r"\end{center}");
-    raw.push_str("\n\n");
+    begin_center(&mut raw);
+    write(&mut raw, &format!("hexadecimal={}", hex));
+    end_center(&mut raw);
 
-    raw.push_str(r"\begin{center}");
-    raw.push_str("\n");
-    raw.push_str(r"\begin{tabular}");
+    begin_center(&mut raw);
+    begin_tabular(&mut raw, hex.len());
+    hline(&mut raw);
 
-    raw.push_str("{ ");
-    for _ in 0..hex.len() {
-        raw.push_str(r"|c");
-    }
-    raw.push_str("| }");
-    raw.push_str("\n");
+    let chars: Vec<char> = hex.chars().collect();
+    add_rows(&mut raw, chars.iter());
 
-    raw.push_str(r" \hline");
-    raw.push_str("\n");
+    let pows: Vec<String> = (0..hex.len()).rev().map(|i| format!("16^{}", i)).collect();
+    add_rows(&mut raw, pows.iter());
 
-    for (index, ch) in hex.chars().enumerate() {
-        if index == hex.len() - 1 {
-            raw.push_str(&format!(" {}", ch));
-            raw.push_str(r" \\");
-        } else {
-            raw.push_str(&format!(" {} & ", ch));
-        }
+    let digits: Vec<u32> = hex.chars().map(|c| c.to_digit(16).unwrap()).collect();
+    let mut values: Vec<u128> = Vec::new();
+    for (index, digit) in digits.iter().rev().enumerate() {
+        let value = *digit as u128 * 16_u128.pow(index as u32);
+        values.push(value);
     }
 
-    raw.push_str("\n");
+    values.reverse();
+    add_rows(&mut raw, values.iter());
 
-    for (index, pos) in (0..hex.len()).rev().enumerate() {
-        if index == hex.len() - 1 {
-            raw.push_str(&format!(" 16^{}", pos));
-            raw.push_str(r" \\");
-        } else {
-            raw.push_str(&format!(" 16^{} & ", pos));
-        }
-    }
+    newline(&mut raw);
+    hline(&mut raw);
+    end_tabular(&mut raw);
+    end_center(&mut raw);
 
-    raw.push_str("\n");
+    begin_center(&mut raw);
 
-    let hex_vec: Vec<char> = hex.chars().collect();
-    for (index, pos) in (0..hex.len()).rev().enumerate() {
-        let value: u128 = match hex_vec[index] {
-            'A' => 10 * 16_u128.pow(pos as u32),
-            'B' => 11 * 16_u128.pow(pos as u32),
-            'C' => 12 * 16_u128.pow(pos as u32),
-            'D' => 13 * 16_u128.pow(pos as u32),
-            'E' => 14 * 16_u128.pow(pos as u32),
-            'F' => 15 * 16_u128.pow(pos as u32),
-            '0' => 0 * 16_u128.pow(pos as u32),
-            '1' => 1 * 16_u128.pow(pos as u32),
-            '2' => 2 * 16_u128.pow(pos as u32),
-            '3' => 3 * 16_u128.pow(pos as u32),
-            '4' => 4 * 16_u128.pow(pos as u32),
-            '5' => 5 * 16_u128.pow(pos as u32),
-            '6' => 6 * 16_u128.pow(pos as u32),
-            '7' => 7 * 16_u128.pow(pos as u32),
-            '8' => 8 * 16_u128.pow(pos as u32),
-            '9' => 9 * 16_u128.pow(pos as u32),
-            unknown => {
-                panic!("Error bitch! {}", unknown);
-            }
-        };
+    let mut decimal = 0;
+    for (index, value) in values.iter().enumerate() {
+        decimal += value;
 
         if index == hex.len() - 1 {
-            raw.push_str(&format!(" {}", value));
-            raw.push_str(r" \\");
-        } else {
-            raw.push_str(&format!(" {} & ", value));
-        }
-    }
-
-    raw.push_str("\n");
-    raw.push_str(r" \hline");
-
-    raw.push_str("\n");
-    raw.push_str(r"\end{tabular}");
-    raw.push_str("\n");
-    raw.push_str(r"\end{center}");
-    raw.push_str("\n\n");
-
-    raw.push_str(r"\begin{center}");
-    raw.push_str("\n");
-    let mut final_value = 0;
-    let hex_vec: Vec<char> = hex.chars().collect();
-    for (index, pos) in (0..hex.len()).rev().enumerate() {
-        let value: u128 = match hex_vec[index] {
-            'A' => 10 * 16_u128.pow(pos as u32),
-            'B' => 11 * 16_u128.pow(pos as u32),
-            'C' => 12 * 16_u128.pow(pos as u32),
-            'D' => 13 * 16_u128.pow(pos as u32),
-            'E' => 14 * 16_u128.pow(pos as u32),
-            'F' => 15 * 16_u128.pow(pos as u32),
-            '0' => 0 * 16_u128.pow(pos as u32),
-            '1' => 1 * 16_u128.pow(pos as u32),
-            '2' => 2 * 16_u128.pow(pos as u32),
-            '3' => 3 * 16_u128.pow(pos as u32),
-            '4' => 4 * 16_u128.pow(pos as u32),
-            '5' => 5 * 16_u128.pow(pos as u32),
-            '6' => 6 * 16_u128.pow(pos as u32),
-            '7' => 7 * 16_u128.pow(pos as u32),
-            '8' => 8 * 16_u128.pow(pos as u32),
-            '9' => 9 * 16_u128.pow(pos as u32),
-            unknown => {
-                panic!("Error bitch! {}", unknown);
-            }
-        };
-
-        final_value += value;
-
-        if index == hex.len() - 1 {
-            raw.push_str(&format!("{} = {}", value, final_value));
+            raw.push_str(&format!("{} = {}", value, decimal));
             raw.push_str(r" \\");
             raw.push_str("\n");
-            raw.push_str(&format!("decimal = {}", final_value));
+            raw.push_str(&format!("decimal = {}", decimal));
         } else {
             raw.push_str(&format!("{} + ", value));
         }
     }
 
-    raw.push_str("\n");
-    raw.push_str(r"\end{center}");
-    raw.push_str("\n\n");
+    newline(&mut raw);
+    end_center(&mut raw);
 
     println!("{}", raw)
+}
+
+pub fn hex_to_octal(hex: &str) {
+    let mut raw = String::new();
+    write(&mut raw, &format!("hexadecimal={}", hex));
+
+    let decimal = u128::from_str_radix(hex, 16).unwrap();
+    let mut cociente = decimal;
+    loop {
+        raw.push_str(r"\opidiv{");
+        raw.push_str(&cociente.to_string());
+        raw.push_str("}{");
+        raw.push_str(&8.to_string());
+        raw.push_str("}");
+        raw.push_str(r" \quad");
+        newline(&mut raw);
+
+        cociente = cociente / 8;
+        if cociente < 8 {
+            break;
+        }
+    }
+
+    write(&mut raw, &format!("octal={:o}", decimal));
+
+    println!("{}", raw)
+}
+
+pub fn write(raw: &mut String, msg: &str) {
+    raw.push_str(&format!("{}", msg));
+    raw.push_str(r" \\");
+    newline(raw);
+}
+
+pub fn begin_center(raw: &mut String) {
+    raw.push_str(r"\begin{center}");
+    raw.push_str("\n");
+}
+
+pub fn end_center(raw: &mut String) {
+    raw.push_str(r"\end{center}");
+    raw.push_str("\n\n");
+}
+
+pub fn begin_tabular(raw: &mut String, colums: usize) {
+    raw.push_str(r"\begin{tabular}");
+
+    raw.push_str("{ ");
+    for _ in 0..colums {
+        raw.push_str(r"|c");
+    }
+    raw.push_str("| }");
+    raw.push_str("\n");
+}
+
+pub fn add_rows<T: std::fmt::Display>(raw: &mut String, iter: Iter<T>) {
+    let len = iter.len();
+    for (index, item) in iter.enumerate() {
+        if index == len - 1 {
+            raw.push_str(&format!(" {}", item));
+            raw.push_str(r" \\");
+        } else {
+            raw.push_str(&format!(" {} & ", item));
+        }
+    }
+
+    newline(raw);
+}
+
+pub fn end_tabular(raw: &mut String) {
+    raw.push_str(r"\end{tabular}");
+    raw.push_str("\n");
+}
+
+pub fn newline(raw: &mut String) {
+    raw.push_str("\n");
+}
+
+pub fn hline(raw: &mut String) {
+    raw.push_str(r" \hline");
+    raw.push_str("\n");
 }
